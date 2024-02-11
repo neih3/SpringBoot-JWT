edc05993f3441693c5b292d2ae80b71ed1a8458f
neih3 <hiencastoo@gmail.com>
2024-02-11 10:08:30 +0700

    first commit

---
+++ src/main/java/com/techsoft/springsecurity/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.techsoft.springsecurity.controller;
+
+import com.techsoft.springsecurity.entity.AuthRequest;
+import com.techsoft.springsecurity.entity.UserInfo;
+import com.techsoft.springsecurity.service.JwtService;
+import com.techsoft.springsecurity.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.security.sasl.AuthenticationException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+    @Autowired
+    private UserInfoService userInfoService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Spring Security tutorials !!";
+    }
+
+    @PostMapping("/login")
+    public String addUser(@RequestBody AuthRequest authRequest){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUserName());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
+//
+
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody UserInfo userInfo){
+        return userInfoService.addUser(userInfo);
+
+    }
+
+    @GetMapping("/getUsers")
+    @PreAuthorize("hasAuthority('ADMIN_ROLES')")
+    public List<UserInfo> getAllUsers(){
+        return userInfoService.getAllUser();
+    }
+    @GetMapping("/getUsers/{id}")
+    @PreAuthorize("hasAuthority('USER_ROLES')")
+    public UserInfo getAllUsers(@PathVariable Integer id){
+        return userInfoService.getUser(id);
+    }
+}
---
+++ src/main/java/com/techsoft/springsecurity/service/UserInfoDetails.java
@@ -0,0 +1,65 @@
+package com.techsoft.springsecurity.service;
+
+import com.techsoft.springsecurity.entity.UserInfo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoDetails implements UserDetails {
+    String userName=null;
+    String password = null;
+    List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(UserInfo userInfo){
+        userName = userInfo.getName();
+        password = userInfo.getPassword();
+        if (userInfo.getRoles() != null) {
+            authorities = Arrays.stream(userInfo.getRoles().split(","))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        } else {
+            authorities = Collections.emptyList();
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
